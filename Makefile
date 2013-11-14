99b7206d47b92a017645100bf71ebe4608c693f6
illerucis <rob.sicurelli@gmail.com>
2013-11-13 19:51:35 -0500

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc tmzdemo1.c gridtmz.c ricker.c snapshot2d.c updatetmz.c -o tmzdemo
---
+++ tmzdemo1.c
@@ -0,0 +1,40 @@
+/*TMz simulation with Ricker source at center of grid.*/
+
+#include "fdtd-alloc1.h"
+#include "fdtd-macro-tmz.h"
+#include "fdtd-proto1.h"
+#include "ezinc.h"
+
+int main()
+{
+    Grid *g;
+
+    // allocate memory for Grid
+    ALLOC_1D(g, 1, Grid);
+
+    // initialize the grid
+    gridInit(g);
+    ezIncInit(g);
+
+    // initialize snapshots
+    snapshotInit2d(g);
+
+    /* do time stepping */
+    for (Time = 0; Time < MaxTime; Time++) {
+        // update magnetic field
+        updateH2d(g);
+        // update electric field
+        updateE2d(g);
+
+        // add a source
+        int index = (SizeX / 2) * SizeYG(g) + (SizeY / 2);
+        g->ez[index] = ezInc(Time, 0.0);
+
+        //Ez(SizeX / 2, SizeY / 2) = ezInc(Time, 0.0);
+        // take a snapshot (if appropriate)
+        snapshot2d(g);
+    }
+
+    return 0;
+
+}
